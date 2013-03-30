c62997bbdaf9af12616ca5bc5658a3708384ffae
Evgeniy Polyakov <zbr@ioremap.net>
2013-03-30 22:07:29 +0400

    Added proper download manager, url/text parser and basic recursion processor

--- test/rindex.cpp
+++ test/rindex.cpp
@@ -3,13 +3,17 @@
 
 #include <unistd.h>
 
+#include <atomic>
+#include <cstring>
 #include <fstream>
-#include <vector>
+#include <functional>
 #include <sstream>
 #include <thread>
-#include <atomic>
+#include <vector>
 
 #include <boost/program_options.hpp>
+
+#include <libxml/HTMLparser.h>
 
 #define EV_MULTIPLICITY 1
 #define EV_MINIMAL       0
@@ -71,7 +75,6 @@ class storage : public elliptics::node {
 class downloader {
 	public:
 		downloader() :  m_async(m_loop), m_manager(m_loop), m_thread(std::bind(&downloader::crawl, this)) {
-			std::cout << "created new downloader object" << std::endl;
 		}
 
 		~downloader() {
@@ -105,15 +108,125 @@ class downloader {
 		}
 
 		void crawl_stop(ev::async &aio, int) {
-			std::cout << "async message received" << std::endl;
 			aio.loop.unloop();
 		}
 };
 
-class process {
+class parser {
 	public:
-		process(int tnum) : m_signal(m_loop), m_downloaders(tnum) {
-			m_signal.set<process, &process::signal_received>(this);
+		parser() : m_process_flag(0) {}
+
+		void parse(const std::string &page) {
+			htmlParserCtxtPtr ctxt;
+
+			htmlSAXHandler handler;
+			memset(&handler, 0, sizeof(handler));
+
+			handler.startElement = static_parser_start_element;
+			handler.endElement = static_parser_end_element;
+			handler.characters = static_parser_characters;
+
+			ctxt = htmlCreatePushParserCtxt(&handler, this, "", 0, "", XML_CHAR_ENCODING_NONE);
+
+			htmlParseChunk(ctxt, page.c_str(), page.size(), 0);
+			htmlParseChunk(ctxt, "", 0, 1);
+
+			htmlFreeParserCtxt(ctxt);
+		}
+
+		std::vector<std::string> &urls(void) {
+			return m_urls;
+		}
+
+		std::vector<std::string> &tokens(void) {
+			return m_tokens;
+		}
+
+
+		void parser_start_element(const xmlChar *tag_name, const xmlChar **attributes) {
+			const char *tag = reinterpret_cast<const char*>(tag_name);
+
+			if (strcasecmp(tag, "a") == 0)
+				return parse_a(attributes);
+
+			update_process_flag(tag, +1);
+		}
+
+		void parser_characters(const xmlChar *ch, int len) {
+			if (m_process_flag <= 0)
+				return;
+
+			m_tokens.emplace_back(reinterpret_cast<const char *>(ch), len);
+		}
+
+		void parser_end_element(const xmlChar *tag_name) {
+			const char *tag = reinterpret_cast<const char*>(tag_name);
+			update_process_flag(tag, -1);
+		}
+
+	private:
+		std::vector<std::string> m_urls;
+		std::vector<std::string> m_tokens;
+		int m_process_flag;
+
+		void update_process_flag(const char *tag, int offset) {
+			static std::string bad_elements[] = {"script", "style"};
+			static std::string good_elements[] = {"body"};
+
+			for (auto bad : bad_elements) {
+				if (strcasecmp(tag, bad.c_str()) == 0) {
+					m_process_flag -= offset;
+					return;
+				}
+			}
+
+			for (auto good : good_elements) {
+				if (strcasecmp(tag, good.c_str()) == 0) {
+					m_process_flag += offset;
+					return;
+				}
+			}
+		}
+
+		static void static_parser_start_element(void *ctx,
+						 const xmlChar *tag_name,
+						 const xmlChar **attributes) {
+			parser *context = reinterpret_cast<parser *>(ctx);
+			context->parser_start_element(tag_name, attributes);
+		}
+
+		static void static_parser_end_element(void *ctx, const xmlChar *tag_name) {
+			parser *context = reinterpret_cast<parser *>(ctx);
+			context->parser_end_element(tag_name);
+		}
+
+		static void static_parser_characters(void *ctx, const xmlChar *ch, int len) {
+			parser *context = reinterpret_cast<parser *>(ctx);
+			context->parser_characters(ch, len);
+		}
+
+		void parse_a(const xmlChar **attributes) {
+			if (!attributes)
+				return;
+
+			for (size_t index = 0; attributes[index]; index += 2) {
+				const xmlChar *name = attributes[index];
+				const xmlChar *value = attributes[index + 1];
+
+				if (!value)
+					continue;
+
+				if (strcmp(reinterpret_cast<const char*>(name), "href") == 0) {
+					m_urls.push_back(reinterpret_cast<const char*>(value));
+				}
+			}
+		}
+};
+
+class dmanager {
+	public:
+		dmanager(int tnum) : m_signal(m_loop), m_downloaders(tnum) {
+			m_signal.set<dmanager, &dmanager::signal_received>(this);
 			m_signal.start(SIGTERM);
 
 			srand(time(NULL));
@@ -137,26 +250,107 @@ class process {
 			m_downloaders[rand() % m_downloaders.size()].enqueue(request, handler);
 		}
 
-		void url_complete(const ioremap::swarm::network_reply &reply) {
+	private:
+		ev::default_loop m_loop;
+		ev::sig m_signal;
+		std::vector<wookie::downloader>	m_downloaders;
+
+		void signal_received(ev::sig &sig, int ) {
+			sig.loop.break_loop();
+		}
+};
+
+namespace url {
+	enum recursion {
+		none = 1,
+		within_domain,
+		full
+	};
+}
+
+class recursor {
+	public:
+		recursor(const std::string &url, const url::recursion recursion, dmanager &d) :
+		m_recursion(recursion), m_dmanager(d), m_downloaded(0) {
+			ioremap::swarm::network_url base_url;
+
+			if (!base_url.set_base(url))
+				throw ioremap::elliptics::error(-EINVAL, "Invalid URL '" + url + "': set-base failed");
+
+			m_base = base_url.host();
+			if (m_base.empty())
+				throw ioremap::elliptics::error(-EINVAL, "Invalid URL '" + url + "': base is empty");
+
+			m_dmanager.feed(url, std::bind(&recursor::process_reply, this, std::placeholders::_1));
+		}
+
+	private:
+		std::string m_base;
+
+		url::recursion m_recursion;
+		dmanager &m_dmanager;
+		std::mutex m_processed_lock;
+		std::set<std::string> m_processed;
+		std::atomic_long m_downloaded;
+
+		void process_reply(const ioremap::swarm::network_reply &reply) {
+			m_downloaded++;
+
+			parser p;
+			p.parse(reply.data);
+
 			std::cout << "url: " << reply.url <<
 				", code: " << reply.code <<
 				", error: " << reply.error <<
 				", data-size: " << reply.data.size() <<
+				", urls: " << p.urls().size() <<
+				", token strings: " << p.tokens().size() <<
+				", requested urls: " << m_processed.size() <<
+				", downloaded urls: " << m_downloaded <<
 				std::endl;
 			for (auto h : reply.headers)
 				std::cout << h.first << " : " << h.second << std::endl;
+
+			switch (m_recursion) {
+			case url::none:
+				return;
+
+			case url::full:
+			case url::within_domain: {
+            			ioremap::swarm::network_url received_url;
+
+				if (!received_url.set_base(reply.url))
+					break;
+
+				for (auto url : p.urls()) {
+					if (url.compare(0, 2, "//") == 0)
+						continue;
+					if (url.compare(0, 7, "mailto:") == 0)
+						continue;
+
+					std::string host;
+					std::string request_url = received_url.relative(url, &host);
+
+					if (request_url.empty() || host.empty())
+						continue;
+
+					if ((m_recursion == url::within_domain) && (host != m_base))
+						continue;
+
+					{
+						std::lock_guard<std::mutex> guard(m_processed_lock);
+						if (m_processed.find(request_url) != m_processed.end())
+							continue;
+
+						m_processed.insert(request_url);
+
+						m_dmanager.feed(request_url, std::bind(&recursor::process_reply, this, std::placeholders::_1));
+					}
+				}
+				}
+			}
 		}
 
-
-	private:
-		ev::default_loop	m_loop;
-		ev::sig			m_signal;
-		std::vector<wookie::downloader>	m_downloaders;
-
-		void signal_received(ev::sig &sig, int rev) {
-			std::cout << "received signal: " << rev << std::endl;
-			sig.loop.break_loop();
-		}
 };
 
 }} /* namespace ioremap::wookie */
@@ -216,7 +410,8 @@ int main(int argc, char *argv[])
 	st.set_groups(groups);
 	st.process_file(key, file);
 
-	wookie::process p(3);
-	p.feed(url, std::bind(&wookie::process::url_complete, &p, std::placeholders::_1));
-	p.start();
+	wookie::dmanager process(3);
+	wookie::recursor rec(url, wookie::url::within_domain, process);
+
+	process.start();
 }
