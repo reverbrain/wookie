af55bdb0d0df0ed6a5e1ccb5d8802ba27ede38c1
Evgeniy Polyakov <zbr@ioremap.net>
2013-05-24 01:18:56 +0400

    Added elliptics_client into list of elliptics libs

--- cmake/Modules/Findelliptics.cmake
+++ cmake/Modules/Findelliptics.cmake
@@ -1,8 +1,9 @@
 find_path(ELLIPTICS_INCLUDE_DIR elliptics/cppdef.h PATHS ${ELLIPTICS_PREFIX}/include /usr/include)
 
 find_library(ELLIPTICS_cpp_LIBRARY NAMES elliptics_cpp PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
+find_library(ELLIPTICS_client_LIBRARY NAMES elliptics_client PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 
-set(ELLIPTICS_LIBRARIES ${ELLIPTICS_cpp_LIBRARY})
+set(ELLIPTICS_LIBRARIES ${ELLIPTICS_cpp_LIBRARY} ${ELLIPTICS_client_LIBRARY})
 set(ELLIPTICS_INCLUDE_DIRS ${ELLIPTICS_INCLUDE_DIR})
 
 message("elliptics: libs: ${ELLIPTICS_LIBRARIES}, includes: ${ELLIPTICS_INCLUDE_DIRS}")
