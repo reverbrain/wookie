0936fbb78056ce27f6883ea58ce2ba298a347ad5
Evgeniy Polyakov <zbr@ioremap.net>
2013-08-02 00:32:37 +0400

    Added license text and default constructor

--- include/wookie/document.hpp
+++ include/wookie/document.hpp
@@ -1,3 +1,21 @@
+/*
+ * 2013+ Copyright (c) Evgeniy Polyakov <zbr@ioremap.net>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __WOOKIE_DOCUMENT_HPP
+#define __WOOKIE_DOCUMENT_HPP
+
 #include <time.h>
 
 #include <ostream>
@@ -6,9 +24,6 @@
 
 #include <elliptics/cppdef.h>
 #include <elliptics/debug.hpp>
-
-#ifndef __WOOKIE_DOCUMENT_HPP
-#define __WOOKIE_DOCUMENT_HPP
 
 namespace ioremap { namespace wookie {
 
@@ -21,6 +36,10 @@ struct document {
 	enum {
 		version = 1,
 	};
+
+	document() {
+		dnet_current_time(&ts);
+	}
 };
 
 }}
