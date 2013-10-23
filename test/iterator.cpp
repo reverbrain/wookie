4c6a5d259358cabc8a601f1c2e85a9a0834ce205
Evgeniy Polyakov <zbr@ioremap.net>
2013-10-23 05:42:02 +0400

    iterator: updated to the latest ioremap::warp::trie with recursive inexact match

--- test/iterator.cpp
+++ test/iterator.cpp
@@ -3,8 +3,11 @@
 #include "wookie/document.hpp"
 
 #include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include <iostream>
+
+#include "warp/lex.hpp"
 
 using namespace ioremap;
 
@@ -15,12 +18,15 @@ int main(int argc, char *argv[])
 	std::string url;
 	variables_map vm;
 	bool text = false;
+	std::string msgin, gram;
 
 	wookie::engine engine;
 
 	engine.add_options("Document reader options")
 		("url", value<std::string>(&url), "Fetch object from storage by URL")
 		("text", "Iterate over text objects, not HTML")
+		("msgpack-input", value<std::string>(&msgin), "Path to Zaliznyak dictionary in msgpacked format")
+		("grammar", value<std::string>(&gram), "Grammar string suitable for ioremap::warp::parser, space separates single word descriptions")
 	;
 
 	try {
@@ -65,10 +71,69 @@ int main(int argc, char *argv[])
 		}
 
 		elliptics::async_read_result bres = data_sess.bulk_read(urls);
+
+		if (!msgin.size() || !gram.size()) {
+			for (const auto &b : bres) {
+				wookie::document doc = wookie::storage::unpack_document(b.file());
+				std::cout << doc << std::endl;
+			}
+
+			return 0;
+		}
+
+		boost::locale::generator gen;
+		std::locale loc = gen("en_US.UTF8");
+
+		ioremap::warp::lex l(loc);
+		l.load(msgin);
+
+		std::vector<std::string> tokens;
+		std::istringstream iss(gram);
+		std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::back_inserter<std::vector<std::string>>(tokens));
+
+		std::vector<ioremap::warp::parsed_word::feature_mask> vgram = l.generate(tokens);
+
 		for (const auto &b : bres) {
 			wookie::document doc = wookie::storage::unpack_document(b.file());
-			std::cout << doc.data << std::endl;
+
+			std::vector<std::string> sentences;
+			boost::split(sentences, doc.data, boost::is_any_of("|"));
+
+			std::cout << doc << ", sentences: " << sentences.size() << std::endl;
+
+			for (auto & sent : sentences) {
+				lb::ssegment_index wmap(lb::word, sent.begin(), sent.end(), loc);
+				wmap.rule(lb::word_any);
+
+				std::vector<std::string> words;
+				for (auto it = wmap.begin(), e = wmap.end(); it != e; ++it) {
+					words.push_back(boost::locale::to_lower(it->str(), loc));
+				}
+
+				for (auto pos : l.grammar(vgram, words)) {
+					for (size_t i = 0; i < vgram.size(); ++i) {
+						std::cout << words[i + pos] << " ";
+					}
+
+					std::cout << std::endl;
+					for (size_t i = 0; i < vgram.size(); ++i) {
+						std::string & word = words[i + pos];
+
+						auto lres = l.lookup(word);
+
+						std::cout << "word: " << word << ": ";
+						for (auto v : lres) {
+							std::cout << v.ending_len <<
+								"," << std::hex << "0x" << v.features <<
+								" " << std::dec;
+						}
+						std::cout << std::endl;
+					}
+					std::cout << std::endl;
+				}
+			}
 		}
+
 	} catch (const std::exception &e) {
 		std::cerr << "Caught exception: " << e.what() << std::endl;
 	}
