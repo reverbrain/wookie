b45e980aa9ffe22f45a44c83580dc817c14762b4
Evgeniy Polyakov <zbr@ioremap.net>
2014-02-03 20:26:00 +0400

    hash: moved murmur hash into separate header

---
+++ include/wookie/hash.hpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013+ Evgeniy Polyakov <zbr@ioremap.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __WOOKIE_HASH_HPP
+#define __WOOKIE_HASH_HPP
+
+namespace ioremap { namespace wookie { namespace hash {
+
+static long murmur(const std::string &str, long seed) const
+{
+	const uint64_t m = 0xc6a4a7935bd1e995LLU;
+	const int r = 47;
+
+	long h = seed ^ (str.size() * m);
+
+	const uint64_t *data = (const uint64_t *)str.data();
+	const uint64_t *end = data + (str.size() / 8);
+
+	while (data != end) {
+		uint64_t k = *data++;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h ^= k;
+		h *= m;
+	}
+
+	const unsigned char *data2 = (const unsigned char *)data;
+
+	switch (str.size() & 7) {
+	case 7: h ^= (uint64_t)data2[6] << 48;
+	case 6: h ^= (uint64_t)data2[5] << 40;
+	case 5: h ^= (uint64_t)data2[4] << 32;
+	case 4: h ^= (uint64_t)data2[3] << 24;
+	case 3: h ^= (uint64_t)data2[2] << 16;
+	case 2: h ^= (uint64_t)data2[1] << 8;
+	case 1: h ^= (uint64_t)data2[0];
+		h *= m;
+	};
+
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+
+	return h;
+}
+
+}}} // namespace ioremap::wookie::hash
+
+#endif /* __WOOKIE_HASH_HPP */
--- test/similarity.hpp
+++ test/similarity.hpp
@@ -1,6 +1,7 @@
 #ifndef __WOOKIE_SIMILARITY_HPP
 #define __WOOKIE_SIMILARITY_HPP
 
+#include "wookie/hash.hpp"
 #include "wookie/parser.hpp"
 #include "wookie/lexical_cast.hpp"
 #include "wookie/timer.hpp"
@@ -170,55 +171,13 @@ class document_parser {
 					std::ostringstream ss;
 					std::copy(ngram.begin(), ngram.end(), std::ostream_iterator<std::string>(ss, ""));
 
-					hashes.emplace_back(hash(ss.str(), 0));
+					hashes.emplace_back(wookie::hash::murmur(ss.str(), 0));
 					ngram.pop_front();
 				}
 			}
 
 			std::set<long> tmp(hashes.begin(), hashes.end());
 			hashes.assign(tmp.begin(), tmp.end());
-		}
-
-
-		// murmur hash
-		long hash(const std::string &str, long seed) const {
-			const uint64_t m = 0xc6a4a7935bd1e995LLU;
-			const int r = 47;
-
-			long h = seed ^ (str.size() * m);
-
-			const uint64_t *data = (const uint64_t *)str.data();
-			const uint64_t *end = data + (str.size() / 8);
-
-			while (data != end) {
-				uint64_t k = *data++;
-
-				k *= m;
-				k ^= k >> r;
-				k *= m;
-
-				h ^= k;
-				h *= m;
-			}
-
-			const unsigned char *data2 = (const unsigned char *)data;
-
-			switch (str.size() & 7) {
-			case 7: h ^= (uint64_t)data2[6] << 48;
-			case 6: h ^= (uint64_t)data2[5] << 40;
-			case 5: h ^= (uint64_t)data2[4] << 32;
-			case 4: h ^= (uint64_t)data2[3] << 24;
-			case 3: h ^= (uint64_t)data2[2] << 16;
-			case 2: h ^= (uint64_t)data2[1] << 8;
-			case 1: h ^= (uint64_t)data2[0];
-				h *= m;
-			};
-
-			h ^= h >> r;
-			h *= m;
-			h ^= h >> r;
-
-			return h;
 		}
 };
 
